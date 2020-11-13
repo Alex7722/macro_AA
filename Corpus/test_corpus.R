1ccf2abb04693a16bf9069f289b36c608ce4c9bc
aurelien <agoutsmedt@hotmail.fr>
2020-11-13 08:38:49 +0000

    Add of a script for testing data

--- Corpus/test_corpus.R
+++ Corpus/test_corpus.R
@@ -1,12 +1,16 @@
-# loading the graph if necessary
+# Elements for a function to test the doublons in nodes
+
+# loading the relevant data
 test_names <- coauthorship_nodes_1990s %>%
   select(citing_author) %>%
   rename(Id = citing_author) %>%
   as.data.table()
 
+# Isolating surname, initials, and first initial
 test_names <- test_names[order(Id)]
 test_names <- test_names[, surname:=  gsub("-.*","",Id)][, initials:=  gsub(".*-","",Id)]
-
 test_names <- test_names[, length_initial:=  nchar(initials), by = "Id"]
 test_names <- test_names[, initial_1:=  gsub("(?<!^).", "", initials, perl=TRUE)]
+
+# Only keep the surname + first initial that occurs more than one time
 test_names <- test_names[, N := .N, by = c("surname","initial_1")][N > 1]
